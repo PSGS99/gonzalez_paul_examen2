b78e2c51dae66e0158974c903985811a90fa21d6
PSGS99 <pgonzalez4756@uta.edu.ec>
2022-02-09 19:45:25 -0500

    Gonzalez Paul 19:45

--- src/screens/Signin/SignupScreen.js
+++ src/screens/Signin/SignupScreen.js
@@ -34,6 +34,34 @@ const SigninScreen = ({ route, navigation }) => {
     return isTrue
   }*/
 
+  function validar(cad) {
+    var total = 0;
+    var longitud = cad.length;
+    var longcheck = longitud - 1;
+    var valida = false;
+
+    if (cad !== "" && longitud === 10) {
+      for (let i = 0; i < longcheck; i++) {
+        if (i % 2 === 0) {
+          var aux = cad.charAt(i) * 2;
+          if (aux > 9) aux -= 9;
+          total += aux;
+        } else {
+          total += parseInt(cad.charAt(i));
+        }
+      }
+
+      total = total % 10 ? 10 - (total % 10) : 0;
+
+      if (cad.charAt(longitud - 1) == total) {
+        valida = true;
+      } else {
+        valida = false;
+      }
+    }
+    return valida;
+  }
+
   function notifyMessage(msg) {
     Alert.alert("Aviso", msg, [
       { text: "OK", onPress: () => console.log("OK Pressed") },
